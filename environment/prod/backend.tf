ced28c58107769b4da4fbe7287012231f09afafd
megha6899 <megha.sharma6899@gmail.com>
2025-12-06 01:30:54 +0530

    Update backend.tf

--- environment/prod/backend.tf
+++ environment/prod/backend.tf
@@ -6,7 +6,7 @@ terraform {
     key                  = "prod.terraform.tfstate"
    access_key = "TCglQLOPtYJ9ZAq1TyjLMUbCc9B+SH9Iz1WWBdYmN8YsB0hfMmvcxkh3H4JySQ2HM/79DtudAqa6+AStmrnKvw=="
     subscription_id      = "5ed71b72-e131-4c8d-8029-6d14191c6dfe"
- 
+  tenant_id = "4616d612-8bc9-4839-bf28-e030beab3250"
   }
 }
 
