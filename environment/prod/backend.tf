c2a5615ed3c3117bd2068abf02bb95935eb9e889
megha6899 <megha.sharma6899@gmail.com>
2025-12-06 00:32:12 +0530

    Enable access_key in backend.tf
    Uncomment access_key for Terraform backend configuration.

--- environment/prod/backend.tf
+++ environment/prod/backend.tf
@@ -4,7 +4,7 @@ terraform {
     storage_account_name = "statestg030"
     container_name       = "statecon030"
     key                  = "prod.terraform.tfstate"
-    # access_key = "TCglQLOPtYJ9ZAq1TyjLMUbCc9B+SH9Iz1WWBdYmN8YsB0hfMmvcxkh3H4JySQ2HM/79DtudAqa6+AStmrnKvw=="
+   access_key = "TCglQLOPtYJ9ZAq1TyjLMUbCc9B+SH9Iz1WWBdYmN8YsB0hfMmvcxkh3H4JySQ2HM/79DtudAqa6+AStmrnKvw=="
     
   }
 }
